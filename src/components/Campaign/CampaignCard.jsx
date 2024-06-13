6f6d0dd740a79616993620eb17da4210e428b846
Aerossky <92148435+Aerossky@users.noreply.github.com>
2024-06-14 00:53:32 +0700

    Update CampaignCard.jsx
    menambahkan style

--- src/components/Campaign/CampaignCard.jsx
+++ src/components/Campaign/CampaignCard.jsx
@@ -11,16 +11,18 @@ const Campaign = ({ id, title, image, content, createdAt, user }) => {
         window.scrollTo(0, 0);
       }}
     >
-      <div className="shadow-lg max-w-sm rounded-2xl">
-        <img
-          className="w-full h-52 rounded-t-2xl shadow-lg"
-          src={`${config.baseUrl}/images/campaigns/${image}`}
-          alt="Gambar kampanye"
-        />
+      <div className="p-4 shadow-lg transition-all duration-500 hover:shadow-xl rounded-2xl">
+        <div className="overflow-hidden">
+          <img
+            className="w-full h-52 rounded-t-2xl shadow-lg object-cover transition duration-700 hover:skew-x-2 hover:scale-110"
+            src={`${config.baseUrl}/images/campaigns/${image}`}
+            alt="Gambar kampanye"
+          />
+        </div>
         <div className="p-4 pb-5 text-center">
           <p className="text-gray-400 pb-2">by {user.username}</p>
-          <h1 className="text-base font-semibold text-center">{title}</h1>
-          <p className="mt-2 text-sm">{content}</p>
+          <h1 className="text-base font-semibold">{title}</h1>
+          <p className="mt-2 text-sm line-clamp-2">{content}</p>
           <p className="text-gray-400 mt-2">{convertDate(createdAt)}</p>
         </div>
       </div>
